5e388b974535f3a25d518067b09283d8767b4197
David Jiang <djiang86@binghamton.edu>
2018-05-16 02:24:12 +0000

    started inventory app

---
+++ Angular4-Inventory-App/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="inventory-app">
+      (product will go here soon)
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
