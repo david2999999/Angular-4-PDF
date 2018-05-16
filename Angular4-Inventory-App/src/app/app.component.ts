c4750c8160da6c07c1c3167d4a92f3c7d98ba393
David Jiang <djiang86@binghamton.edu>
2018-05-16 02:48:45 +0000

    creating list of products

--- Angular4-Inventory-App/src/app/app.component.ts
+++ Angular4-Inventory-App/src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="inventory-app">
-      (product will go here soon)
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'NICEHAT',
+        'Black Hat',
+        'https://cdn.shopify.com/s/files/1/1898/2583/products/CA-U-_-BLK-BRJM001__167_31fb0c06-' +
+                '77bb-409d-8c09-cf110a910f16_1024x1024.jpg?v=1492635434',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        'https://images-na.ssl-images-amazon.com/images/I/915rL8htN0L._SY400_.jpg',
+        ['Men', 'Apparel', 'Jacket & Vest'],
+        200.99
+        ),
+      new Product(
+        'EARING',
+        'Diamond Earing',
+        'https://bnsec.bluenile.com/bluenile/is/image/bluenile/-' +
+        'diamond-halo-earrings-platinum-/50674_main?rgn=343,535,1224,792&$v2_phab_detailmain_lrg$',
+        ['Women', 'Accessories','Gems'],
+        999.99
+      )
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ' + product);
+  }
 }
