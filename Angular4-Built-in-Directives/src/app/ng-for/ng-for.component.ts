f1fd0c8db391e7998829b7abeb9a8e0c83c8b7f4
David Jiang <djiang86@binghamton.edu>
2018-05-19 02:55:22 +0000

    rendering nested data

--- Angular4-Built-in-Directives/src/app/ng-for/ng-for.component.ts
+++ Angular4-Built-in-Directives/src/app/ng-for/ng-for.component.ts
@@ -8,15 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class NgForComponent implements OnInit {
   cities: string[];
   people: object[];
+  peopleByCity: object[];
 
   constructor() { }
 
   ngOnInit() {
     this.cities = ['Miami', 'Sao Paulo', 'New York'];
+
     this.people = [
       {name: 'Anderson', age: 35, city: 'Sao Paulo'},
       {name: 'John', age: 12, city: 'Miami'},
       {name: 'Peter', age: 22, city: 'New York'}
+    ];
+
+    this.peopleByCity = [
+      {
+        city: 'Miami',
+        people: [
+          {name: 'John', age: 12},
+          {name: 'Angel', age: 22}
+        ]
+      },
+      {
+        city: 'Sao Paulo',
+        people: [
+          {name: 'Anderson', age: 35},
+          {name: 'Felipe', age: 36}
+        ]
+      }
     ];
   }
 
