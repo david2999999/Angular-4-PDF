4b0aff5836c782ae9aea1452b30910823d7218ad
David Jiang <djiang86@binghamton.edu>
2018-05-25 03:36:59 +0000

    started angular4 HTTP

---
+++ Angular4-HTTP/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SuiModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
