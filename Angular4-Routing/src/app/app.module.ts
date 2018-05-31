360dd3f8e665c01b028f7d59a886a0f359554623
David Jiang <djiang86@binghamton.edu>
2018-05-31 02:17:17 +0000

    created routes in app.modules

--- Angular4-Routing/src/app/app.module.ts
+++ Angular4-Routing/src/app/app.module.ts
@@ -3,6 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {Routes} from '@angular/router';
+
+// • path specifies the URL this route will handle
+// • component is what ties a given route path to a component that will handle the route
+// • the optional redirectTo is used to redirect a given path to an existing route
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'contactus', redirectTo: 'contact'},
+
+  // authentication demo
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [LoggedInGuard]
+  },
+
+  // nested
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: childRoutes
+  }
+]
 
 
 @NgModule({
@@ -10,7 +37,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
